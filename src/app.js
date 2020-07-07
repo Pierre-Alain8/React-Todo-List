2ae6418453aa93fa9c4d58916097cd40ef6ed1c9
Pierre-Alain-agn <pierrealainagn@gmail.com>
2020-07-07 23:12:07 +0200

    home import in app.js update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/home";
 
 class App extends React.Component {
   render() {
