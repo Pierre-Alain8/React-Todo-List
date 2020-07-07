7863ff21411a35ae400cac54896db0f2b67e8eca
Pierre-Alain-agn <pierrealainagn@gmail.com>
2020-07-07 23:08:13 +0200

    home import in app.js update

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import {Route, Switch, BrowserRouter as Router} from 'react-router-dom';
-import Home from './components/Home';
+import React from "react";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home";
 
 class App extends React.Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                </Switch>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
